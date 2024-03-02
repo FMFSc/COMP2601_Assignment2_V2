8cd1d47af9816726df447309c8f0e0fccc87779b
FMFSc Gandalf <fellipescirea@gmail.com>
2024-03-02 15:01:36 -0800

    Implementing the Orderable interface

---
+++ src/code/Orderable.java
@@ -0,0 +1,12 @@
+/**
+ * This Orderable interface will have only two abstract methods that will be implemented in classes.
+ * @Author Fellipe Matheus Fumagali Scirea
+ * @version 2.0
+ */
+
+public interface Orderable
+{
+    public Orderable next();
+
+    public Orderable previous();
+}
