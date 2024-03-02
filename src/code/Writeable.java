74423aa2cb658631544b837f7a377dc8f253fdf8
FMFSc Gandalf <fellipescirea@gmail.com>
2024-03-02 15:10:33 -0800

    Implementing the Writeable interface

---
+++ src/code/Writeable.java
@@ -0,0 +1,17 @@
+/**
+ * This Writeable interface will be a functional interface, therefore, will only have one abstract method.
+ * The implementation is expected to have a suitable application to print the data.
+ * @Author Fellipe Matheus Fumagali Scirea
+ * @version 2.0
+ */
+
+public interface Writeable
+{
+    /**
+     * This abstract method will take arguments and upon implementation, will return a
+     * @param s     String data to be printed.
+     * @param min   minimum value as parameter argument to be printed.
+     * @param max   maximum value as parameter argument to be printed.
+     */
+    public void printData(String s, int min, int max);
+}
